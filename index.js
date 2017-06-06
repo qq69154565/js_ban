27515ccd3b1a8b18c66f9e13d21491bc6804d368
qq69154565 <69154565@qq.com>
2017-06-06 16:45:34 +0800

    first post

---
+++ index.js
@@ -0,0 +1,10 @@
+(function(window,document){  
+    var MaskShare = function(q){  
+  		alert(q)
+    };  
+    MaskShare.prototype = {
+    	
+    };  
+  
+    window.MaskShare = MaskShare;  
+}(window,document));  
